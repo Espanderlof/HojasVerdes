35090b2dd6111b6a3690903782ed2f85c5713796
Fralkayg <Fralkayg@Fralkayg-PC>
2015-12-15 20:02:04 -0300

    Clase cliente terminada

--- src/dominio/cliente.java
+++ src/dominio/cliente.java
@@ -1,14 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dominio;
 
-/**
- *
- * @author Fralkayg
- */
 public class cliente {
+    private int rut_cliente;
+    private String nombre;
+    private String direccion;
+    private String giro;
+    private int telefono;
+    private String contacto;
+    private String razon_social;
+
+    public cliente() {
+    }
+
+    public int getRut_cliente() {
+        return rut_cliente;
+    }
+
+    public void setRut_cliente(int rut_cliente) {
+        this.rut_cliente = rut_cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getGiro() {
+        return giro;
+    }
+
+    public void setGiro(String giro) {
+        this.giro = giro;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    public String getRazon_social() {
+        return razon_social;
+    }
+
+    public void setRazon_social(String razon_social) {
+        this.razon_social = razon_social;
+    }
+    
+    
     
 }
